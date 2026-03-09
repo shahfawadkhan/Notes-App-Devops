c2532cd5ce5e1cf0648b4fa8285c5eb17ec11d5b
shahfawadkhan <sfawadkhan92@gmail.com>
2026-03-10 01:22:22 +0500

    ingress

--- backend/server.js
+++ backend/server.js
@@ -17,4 +17,4 @@ app.use('/api/notes', require('./routes/notes'));
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
